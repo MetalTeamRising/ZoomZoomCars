b35a7be02fb861fd15ca280b6581bb7048977843
Jason Noone <jdn1885@ad.rit.edu>
2016-11-02 08:54:28 -0400

    Added script for checkpoint and Car
    I've started working on the Car script and I've made the base for the
    Checkpoint script that can be tweaked when we have a WorldController

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (Collider))]
+public class Car : MonoBehaviour {
+
+    private GameObject obj;
+    private Rigidbody rBod;
+    [SerializeField]private float speed = 10000000000.0f;
+    [SerializeField]private Vector3 accel;
+    private Vector3 direction;
+    private float turnAngle;
+
+	// Use this for initialization
+	void Start () {
+        obj = gameObject;
+        rBod = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move(Input.GetAxis("Horizontal"));
+	}
+
+    void Move(float move)
+    {
+        direction = Vector3.forward;
+        direction.Normalize();
+        direction *= 0.2f;
+        accel = new Vector3(direction.x, direction.y, direction.z);
+        rBod.velocity = new Vector3(move * speed, rBod.velocity.y, move * speed);
+        
+    }
+}
---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Collider))]
+public class Checkpoint : MonoBehaviour {
+
+    private Collider collide;
+    private int checkpoint;
+
+	// Use this for initialization
+	void Start () {
+        collide = this.GetComponent<Collider>();
+        checkpoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEvent(Collider other)
+    {
+        if(other.tag == "Car")
+        {
+            checkpoint++;
+        }
+    }
+}
